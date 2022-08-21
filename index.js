84eb44178c44b5237ea09de35d86fed2e3cdfeb9
Alex MacArthur <alex@macarthur.me>
2022-08-20 23:26:36 -0500

    Initial commit.

---
+++ index.js
@@ -0,0 +1,37 @@
+import { Router } from 'itty-router'
+
+const router = Router()
+
+router.post('/api/event', async request => {
+    const newRequest = new Request(request)
+    newRequest.headers.delete('cookie')
+
+    return await fetch('https://analytics.macarthur.me/api/event', newRequest)
+})
+
+router.get('/js/numbers.js', async (request, event) => {
+    let response = await caches.default.match(request)
+
+    if (!response) {
+        response = await fetch('https://analytics.macarthur.me/js/plausible.js')
+        const clonedResponse = new Response(response.body, response)
+
+        const WEEK_IN_SECONDS = 604800
+        clonedResponse.headers.set(
+            'Cache-Control',
+            `public, max-age=${WEEK_IN_SECONDS}`
+        )
+
+        event.waitUntil(caches.default.put(request, clonedResponse.clone()))
+
+        return clonedResponse
+    }
+
+    return response
+})
+
+addEventListener('fetch', event => {
+    event.passThroughOnException()
+
+    event.respondWith(router.handle(event.request, event))
+})
