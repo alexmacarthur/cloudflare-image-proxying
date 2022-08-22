afe4005f74405076f377a26832d310e1e235b9cb
Alex MacArthur <alex@macarthur.me>
2022-08-21 22:31:22 -0500

    set up font caching.

--- index.js
+++ index.js
@@ -1,37 +1,55 @@
-import { Router } from 'itty-router'
+import { Router } from "itty-router";
 
-const router = Router()
+const router = Router();
 
-router.post('/api/event', async request => {
-    const newRequest = new Request(request)
-    newRequest.headers.delete('cookie')
+router.post("/api/event", async request => {
+  const newRequest = new Request(request);
+  newRequest.headers.delete("cookie");
 
-    return await fetch('https://analytics.macarthur.me/api/event', newRequest)
-})
+  return await fetch("https://analytics.macarthur.me/api/event", newRequest);
+});
 
-router.get('/js/numbers.js', async (request, event) => {
-    let response = await caches.default.match(request)
+router.get("/**/*.woff2", async (request, event) => {
+  let response = await caches.default.match(request);
 
-    if (!response) {
-        response = await fetch('https://analytics.macarthur.me/js/plausible.js')
-        const clonedResponse = new Response(response.body, response)
+  if (!response) {
+    response = await fetch(request);
+    const clonedResponse = new Response(response.body, response);
 
-        const WEEK_IN_SECONDS = 604800
-        clonedResponse.headers.set(
-            'Cache-Control',
-            `public, max-age=${WEEK_IN_SECONDS}`
-        )
+    // one year
+    clonedResponse.headers.set("Cache-Control", "public, max-age=31560000");
 
-        event.waitUntil(caches.default.put(request, clonedResponse.clone()))
+    event.waitUntil(caches.default.put(request, clonedResponse.clone()));
 
-        return clonedResponse
-    }
+    return clonedResponse;
+  }
 
-    return response
-})
+  return response;
+});
 
-addEventListener('fetch', event => {
-    event.passThroughOnException()
+router.get("/js/numbers.js", async (request, event) => {
+  let response = await caches.default.match(request);
 
-    event.respondWith(router.handle(event.request, event))
-})
+  if (!response) {
+    response = await fetch("https://analytics.macarthur.me/js/plausible.js");
+    const clonedResponse = new Response(response.body, response);
+
+    const WEEK_IN_SECONDS = 604800;
+    clonedResponse.headers.set(
+      "Cache-Control",
+      `public, max-age=${WEEK_IN_SECONDS}`
+    );
+
+    event.waitUntil(caches.default.put(request, clonedResponse.clone()));
+
+    return clonedResponse;
+  }
+
+  return response;
+});
+
+addEventListener("fetch", event => {
+  event.passThroughOnException();
+
+  event.respondWith(router.handle(event.request, event));
+});
