0838b754a12967dca4b4e7bcb73bc7ccecdced32
Alex MacArthur <alex@macarthur.me>
2022-09-02 21:51:57 -0500

    Just use image proxy endpoint.

--- index.ts
+++ index.ts
@@ -6,71 +6,8 @@ interface AppEnv {
 
 const router = Router();
 
-/**
- * Cache static assets.
- */
-router.routes.push([
-  "GET",
-  /.+?\.(css|woff2|svg)/,
-  [
-    async (request, _env, ctx) => {
-      let response = await caches.default.match(request as Request);
-
-      if (!response) {
-        response = await fetch(request as Request);
-        const clonedResponse = new Response(response.body, response);
-
-        // one year
-        clonedResponse.headers.set("Cache-Control", "public, max-age=31560000");
-        clonedResponse.headers.set(
-          "X-Special-Message",
-          "Thanks for visiting Alex's site!"
-        );
-
-        ctx.waitUntil(
-          caches.default.put(request as Request, clonedResponse.clone())
-        );
-
-        return clonedResponse;
-      }
-
-      return response;
-    }
-  ]
-]);
-
-router.get("/js/numbers.js", async (request, _env, ctx) => {
-  let response = (await caches.default.match(request as Request)) as Response;
-
-  if (!response) {
-    response = await fetch("https://analytics.macarthur.me/js/plausible.js");
-    const clonedResponse = new Response(response.body, response);
-
-    const WEEK_IN_SECONDS = 604800;
-    clonedResponse.headers.set(
-      "Cache-Control",
-      `public, max-age=${WEEK_IN_SECONDS}`
-    );
-
-    ctx.waitUntil(
-      caches.default.put(request as Request, clonedResponse.clone())
-    );
-
-    return clonedResponse;
-  }
-
-  return response;
-});
-
-router.post("/api/event", async request => {
-  const newRequest = new Request(request as Request);
-  newRequest.headers.delete("cookie");
-
-  return await fetch("https://analytics.macarthur.me/api/event", newRequest);
-});
-
 router.get(
-  "/proxied-image/:imageId",
+  "/proxy/:imageId",
   async (request: Request, env: AppEnv, ctx: ExecutionContext) => {
     const { imageId } = (request as any).params;
     const cacheKey = new Request(request.url.toString(), request);
@@ -82,6 +19,7 @@ router.get(
     }
 
     console.log(`Cache MISS for ${imageId}`);
+
     const obj = await env.MACARTHUR_ME.get(decodeURIComponent(imageId));
 
     if (!obj) {
